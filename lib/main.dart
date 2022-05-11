e1e6af0f3c9be19e4e028508fd98fca9244a12bc
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-11 16:31:18 +0200

    change the directory with path_provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_list/src/core/my_app.dart';
 import 'package:todo_list/src/modules/home/models/todo_model.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
   //TODO remember what that does
   WidgetsFlutterBinding.ensureInitialized();
-  // final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
-  // Hive.init(appDocumentDirectory.path);
-  await Hive.initFlutter();
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  // await Hive.initFlutter();
   Hive.registerAdapter(TodoModelAdapter());
   final todosBox = await Hive.openBox('todos');
   runApp(MyApp());
