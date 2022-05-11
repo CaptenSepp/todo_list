37352aee17ad95b8805176c10930c37aef74e8b8
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-11 13:18:34 +0200

    hive/hive dependencies added

--- lib/src/core/my_app.dart
+++ lib/src/core/my_app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.red,
       ),
       home: const HomePage(),
     );
--- lib/src/modules/home/providers/home_provider.dart
+++ lib/src/modules/home/providers/home_provider.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:todo_list/src/modules/home/models/todo_model.dart';
 
 class HomeProvider extends ChangeNotifier {
-  //TODO changed todos from initiated to late
-  late List<TodoModel> todos;
+  //TODO changed todos from initiated to late and initiated, without initiating it doesn't run
+  late List<TodoModel> todos = [];
 
 
   void onDelete(UniqueKey id) {
