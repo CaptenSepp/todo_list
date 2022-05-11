99a7013b5327f726e09d55a2b0f27707108f30f7
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-11 16:30:06 +0200

    register adapter, open the box

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_list/src/core/my_app.dart';
+import 'package:todo_list/src/modules/home/models/todo_model.dart';
 
 void main() async {
   //TODO remember what that does
   WidgetsFlutterBinding.ensureInitialized();
-  // final appDocumentDirectory =
-  //     await path_provider.getApplicationDocumentsDirectory();
+  // final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
   // Hive.init(appDocumentDirectory.path);
   await Hive.initFlutter();
-  // final todosBox = await Hive.openBox('todos');
+  Hive.registerAdapter(TodoModelAdapter());
+  final todosBox = await Hive.openBox('todos');
   runApp(MyApp());
 }
