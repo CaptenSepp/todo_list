fcbc5d9ad083e75f66e11b43ce0c52cc6f74dc2a
Asus <alaalfam@gmail.com>
2022-05-13 03:07:22 +0430

    clear main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_list/src/core/db/todo_db.dart';
 import 'package:todo_list/src/core/my_app.dart';
 import 'package:todo_list/src/modules/home/models/todo_model.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
-  //TODO remember what that does
   WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDirectory =
-      await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDirectory.path);
-  // await Hive.initFlutter();
+
+  await Hive.initFlutter();
   Hive.registerAdapter(TodoModelAdapter());
-  final todosBox = await Hive.openBox<TodoModel>('todos');
+  await TodoDB.instance.initialize();
   runApp(const MyApp());
 }
