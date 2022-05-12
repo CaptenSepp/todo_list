dd352034fa388163d44926e8a3730d1c0a67f028
Asus <alaalfam@gmail.com>
2022-05-13 03:09:18 +0430

    create TodoDB with methods

---
+++ lib/src/core/db/todo_db.dart
@@ -0,0 +1,33 @@
+import 'package:hive/hive.dart';
+import 'package:todo_list/src/modules/home/models/todo_model.dart';
+
+class TodoDB {
+  static final TodoDB instance = TodoDB._();
+  final String boxName = 'todoBox';
+  TodoDB._();
+
+  late final Box<TodoModel> _todoBox;
+
+  Future<void> initialize() async {
+    _todoBox = await Hive.openBox<TodoModel>(boxName);
+  }
+
+  //TODO add two method add and get for hiveBox
+  void addTodo(TodoModel newTodo) {
+    _todoBox.put(newTodo.id, newTodo);
+  }
+
+  TodoModel? getTodo(String id) {
+    return _todoBox.get(id);
+  }
+
+  void deleteTodo(String id) {
+    _todoBox.delete(id);
+  }
+
+  List<TodoModel> getAllTodos() {
+    List<TodoModel> todoList = [];
+    todoList.addAll(_todoBox.values);
+    return todoList;
+  }
+}
