057c3943aa45072ca6e73d5262f43c4ef8f7d039
Asus <alaalfam@gmail.com>
2022-05-13 03:14:49 +0430

    write an initializer for todoList

--- lib/src/modules/home/providers/home_provider.dart
+++ lib/src/modules/home/providers/home_provider.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/src/core/db/db.dart';
+import 'package:todo_list/src/core/db/todo_db.dart';
 import 'package:todo_list/src/modules/home/models/todo_model.dart';
-import 'todo_create_provider.dart';
 
 class HomeProvider extends ChangeNotifier {
   //TODO changed todos from initiated to late and initiated, without initiating it doesn't run
+  final todoDB = TodoDB.instance;
+  List<TodoModel>? todoList;
 
+  HomeProvider() {
+    initialize();
+  }
 
-  void onDelete(UniqueKey id) {
-    TodoDB.getInstance().todoGet().removeWhere((todoModel) => todoModel.id == id);
+  void initialize() {
+    todoList = todoDB.getAllTodos();
+    notifyListeners();
+  }
+
+  void onDelete(String id) {
+    todoDB.deleteTodo(id);
     notifyListeners();
   }
 }
