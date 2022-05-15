f028e1c80da6b05e4a4c3e36fe6692c6d0bbbd09
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-15 02:09:21 +0200

    import removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:todo_list/src/core/db/todo_db.dart';
 import 'package:todo_list/src/core/my_app.dart';
+import 'package:todo_list/src/core/todoDB/todo_db.dart';
 import 'package:todo_list/src/modules/home/models/todo_model.dart';
 
 void main() async {
