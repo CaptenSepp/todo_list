4251f37494b3e53477d237189b87fd3220b6927b
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-15 02:11:32 +0200

    push function back to home page added

--- lib/src/modules/home/providers/todo_create_provider.dart
+++ lib/src/modules/home/providers/todo_create_provider.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/src/core/db/todo_db.dart';
+import 'package:todo_list/src/core/todoDB/todo_db.dart';
 import 'package:todo_list/src/modules/home/models/todo_model.dart';
+import 'package:todo_list/src/modules/home/pages/home_page.dart';
 
 class TodoCreateProvider extends ChangeNotifier {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  void Function(BuildContext context)? push;
   final todoDB = TodoDB.instance;
 
-  void onSave() {
+  void onSave(BuildContext context) {
     String title = titleController.text;
     String? description = descriptionController.text;
     DateTime createdDate = DateTime.now();
@@ -15,12 +17,26 @@ class TodoCreateProvider extends ChangeNotifier {
     TodoModel newTodo = TodoModel(
       title: title,
       description: description,
-      createdDate: createdDate, id: UniqueKey().toString(),
+      createdDate: createdDate,
+      id: UniqueKey().toString(),
     );
     todoDB.addTodo(newTodo);
+
+    //TODO added push to home page here
+    void push(context) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const HomePage(),
+        ),
+      );
+    }
 
     titleController.clear();
     descriptionController.clear();
     notifyListeners();
+    push(context);
+
+
   }
 }
