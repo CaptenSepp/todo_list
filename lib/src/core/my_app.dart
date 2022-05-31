43d2d6bc9cf34cd818320c06d2b5c67c0e28de2b
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-31 13:23:03 +0200

    VRouter and first WVidget in myApp added

--- lib/src/core/my_app.dart
+++ lib/src/core/my_app.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/src/modules/home/pages/home_page.dart';
+import 'package:vrouter/vrouter.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return VRouter(
       debugShowCheckedModeBanner: false,
       title: 'Hive',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const HomePage(),
+      routes: [
+        VWidget(
+          path: '/',
+          widget: const HomePage(),
+        ),
+      ],
     );
   }
 }
